c871328892e3be4ac1371e4a2ee5e609e747b326
Gaurhari <gaurharidass@gmail.com>
2022-09-19 00:48:29 +0100

    project files

---
+++ project/Versions.scala
@@ -0,0 +1,13 @@
+object Versions {
+
+  val spark = "2.3.0"
+  val sparkTestingBase = "2.4.5_0.14.0"
+  val pprint = "0.5.0"
+  val scalaTest = "3.0.3"
+  val scalaMock = "3.5.0"
+  val scalaCheck = "1.13.4"
+  val config = "1.4.2"
+  val scope = "provided"
+  val scalaLogging = "3.5.0"
+  val dataBricksCsv = "1.5.0"
+}
