920e9b62136b5f6d823abc8321cf0d40a9ed922d
Gaurhari <gaurharidass@gmail.com>
2022-09-19 15:28:14 +0100

    updated Constants and removed static values, updated config as well

--- src/main/scala/SparkScalaBcgMainApp.scala
+++ src/main/scala/SparkScalaBcgMainApp.scala
@@ -10,8 +10,17 @@ import org.bgc.uk.service.LoadIMDBData
   */
 object SparkScalaBcgMainApp {
 
+  /** Main function of the application to do all the execution calls
+    *
+    * @param args
+    *   Array of String input
+    */
   def main(args: Array[String]) = {
 
+    /** check for configuration file if passed by user then it will load that
+      * file otherwise load from resources folder
+      */
+    if (args.length > 0 && args(0) != null) DataConfigs.getConfig(args(0))
     LoadIMDBData.getTopNMovies()
     LoadIMDBData.getTitlesOfTopNMovies
     LoadIMDBData.getPersonsMostOftenCredited()
